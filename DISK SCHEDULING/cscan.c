97e84d0eeb24d013efba2cce36d17c7e2ea35967
Ashwin K N <88036631+ashwinkn2004@users.noreply.github.com>
2024-06-29 22:27:43 +0530

    cscan disk scheduling

---
+++ DISK SCHEDULING/cscan.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+
+int head, limit, arr[100], headMovement = 0, currentHead, ch, index, value, maxVal, minVal = 0;
+
+void sort(){
+    for(int i = 0; i < limit; i++){
+        for(int j = 0; j < limit-1-i; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+void trackLeft(){
+    for(int i = index - 1; i >= 0; i--){
+        headMovement += abs(currentHead - arr[i]);
+        currentHead = arr[i];
+        printf(" -> %d", currentHead);
+    }
+    printf(" -> %d",minVal);
+    headMovement += abs(currentHead - minVal);
+    currentHead = minVal;
+    printf(" -> %d",maxVal);
+    headMovement += abs(currentHead - maxVal);
+    currentHead = maxVal;
+    for(int i = limit; i > index ; i--){
+        headMovement += abs(currentHead - arr[i]);
+        currentHead = arr[i];
+        printf(" -> %d", currentHead);
+    }
+}
+
+void trackRight(){
+    for(int i = index; i < limit; i++){
+        headMovement += abs(currentHead - arr[i]);
+        currentHead = arr[i];
+        printf(" -> %d", currentHead);
+    }
+    printf(" -> %d",maxVal);
+    headMovement += abs(currentHead - maxVal);
+    currentHead = maxVal;
+    printf(" -> %d",minVal);
+    headMovement += abs(currentHead - minVal);
+    currentHead = minVal;
+    for(int i = 0; i < index ; i++){
+        headMovement += abs(currentHead - arr[i]);
+        currentHead = arr[i];
+        printf(" -> %d", currentHead);
+    }
+}
+
+void track(){
+    int i;
+    currentHead = head;
+    printf("%d",currentHead);
+    index;
+    for(i = 0; i < limit; i++){
+        if(arr[i] > head){
+            index = i;
+            break;
+        }
+    }
+    if(ch == 0) trackLeft();
+    else trackRight();
+    printf("\nHead Movement = %d\n",headMovement);
+}
+
+void main(){
+    printf("Enter the Head : ");
+    scanf("%d",&head);
+    printf("Enter the limit of elements : ");
+    scanf("%d",&limit);
+    printf("Enter the elements : ");
+    for(int i = 0; i < limit; i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter the max limit : ");
+    scanf("%d",&value);
+    maxVal = value - 1;
+    printf("Track side ? 0(leaft) | 1(right) : ");
+    scanf("%d",&ch);
+    sort();
+    track();
+}
